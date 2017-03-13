44fcc470817f640929bd3434a0cdf87db0442980
Dvshah13 <dvshah13@gmail.com>
2017-03-13 10:20:45 -0500

    Adding basic backend

---
+++ backend.py
@@ -0,0 +1,24 @@
+import csv
+# import pandas as pd
+from yahoo_finance import Share
+
+sma_20day = []
+rsi_20day = []
+
+def current_data(stock):
+    apple = Share(stock)
+    print apple.get_price()
+    print apple.get_volume()
+    print apple.get_50day_moving_avg()
+    print apple.get_price_earnings_ratio()
+    print apple.get_price_earnings_growth_ratio()
+
+current_data('AAPL')
+
+# def get_data(filename):
+# 	with open(filename, 'r') as csvfile:
+# 		csvFileReader = csv.reader(csvfile)
+# 		next(csvFileReader)	# skipping column names
+# 		for row in csvFileReader:
+# 			dates.append(int(row[0].split('-')[0]))
+# 			rsi.append(float(row[1]))
