bd892d4cc11e140bf19a93754b1fece4eff3cae9
Dvshah13 <dvshah13@gmail.com>
2017-03-13 13:49:04 -0500

    Basic mongo db connector

--- backend.py
+++ backend.py
@@ -1,9 +1,10 @@
 import csv
 # import pandas as pd
 from yahoo_finance import Share
+from pymongo import MongoClient
 
-sma_20day = []
-rsi_20day = []
+client = MongoClient('mongodb://localhost:27017')
+db = client.stock_user_info
 
 def current_data(stock):
     if stock == 'apple':
@@ -25,6 +26,36 @@ def data_front_page(stock):
     print stock.get_price_earnings_growth_ratio()
     print stock.get_short_ratio()
 
+def add_user():
+    posts = db.users
+    post_data = {
+        'Name': 'Deepak',
+        'Email': 'dvshah13@gmail.com',
+        'Password': 'test123'
+        }
+    result = posts.insert_one(post_data)
+    print('One post: {0}'.format(result.inserted_id))
+
+def user_portfolio():
+    posts = db.user_portfolio
+    post_data = {
+        'Email': 'dvshah13@gmail.com',
+        'Stock Name': 'apple, google'
+    }
+    result = posts.insert_one(post_data)
+    print('One post: {0}'.format(result.inserted_id))
+
+def stock_filters():
+    posts = db.stock_filters
+    post_data = {
+        'Stock Name': 'apple',
+        'filters': 'various filters'
+    }
+    result = posts.insert_one(post_data)
+    print('One post: {0}'.format(result.inserted_id))
+
+    
+add_user()
 my_stock = 'apple'
 current_data(my_stock)
 
