f09a0f1ece7c462f71870e49980d147ef7f48d42
Dvshah13 <dvshah13@gmail.com>
2017-03-15 10:14:22 -0500

    Adding bcrypt and user

--- api/users.py
+++ api/users.py
@@ -7,7 +7,8 @@ connect('financial_modeling', host='localhost', port=27017)
 
 # Different Mongo collections/tables
 class User(Document):
-    name = StringField(required=True, max_length=200)
-    email = StringField(required=True, unique=True)
-    password = StringField(required=True, max_length=50)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+    email = StringField(required=True)
+    password = StringField(max_length=200)
     cdate = DateTimeField(default=datetime.datetime.now)
