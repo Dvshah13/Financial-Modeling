88aeaee26a15870b485118789957965a02aaebad
Dvshah13 <dvshah13@gmail.com>
2017-03-16 10:10:20 -0500

    Modifying react components

--- react/scripts.js
+++ react/scripts.js
@@ -1,47 +1,51 @@
 /*
-Managing internal state
-Sets the intial state of the component and then,
-when a link is clicked, updates the state.
-When the state updates, the component intelligently
-and efficiently re-renders.
-Note that the `onClick` is the same as the JavaScript
-`onClick` event handler. There are many common browser
-events that are supported by React. See them all at:
-http://facebook.github.io/react/docs/events.html
+Combining components together
+The most fundamental and useful part of React is that
+you can create any number of components and nest them
+just like you would any HTML tag. You pass down data
+to your components from parent components in a one-way
+data flow.
+Note: If you use something like Flux/Reflux you have a bit
+power when it comes to data storage and event handling.
+Using a Flux-like framework with React is very helpful.
 */
 
-var ToggleText = React.createClass({
-  getInitialState: function () {
-    return {
-      showDefault: true
-    }
-  },
-
-  toggle: function (e) {
-    // Prevent following the link.
-    e.preventDefault();
-
-    // Invert the chosen default.
-    // This will trigger an intelligent re-render of the component.
-    this.setState({ showDefault: !this.state.showDefault })
-  },
-
+var ProductItem = React.createClass({
   render: function () {
-    // Default to the default message.
-    var message = this.props.default;
-
-    // If toggled, show the alternate message.
-    if (!this.state.showDefault) {
-      message = this.props.alt;
-    }
-
     return (
-      <div>
-        <h1>Hello {message}!</h1>
-        <a href="" onClick={this.toggle}>Toggle</a>
-      </div>
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.price}</td>
+      </tr>
     );
   }
 });
 
-ReactDOM.render(<ToggleText default="World" alt="Mars" />, document.getElementById('root'));
+var ProductList = React.createClass({
+  render: function () {
+    var products = this.props.products.map(function (product, index) {
+      return (
+        <ProductItem
+          key={index}
+          name={product.name}
+          price={product.price}
+        />
+      );
+    });
+
+    return (
+      <table>
+        {products}
+      </table>
+    );
+  }
+});
+
+// Could come from an API, LocalStorage, another component, etc...
+var products = [
+  { name: 'Toast', price: 1499 },
+  { name: 'Bacon', price: 3245 },
+  { name: 'Coffee', price: 300 }
+];
+
+ReactDOM.render(<ProductList products={products} />, document.getElementById('root'));
