0c0571abdd14f2f164d2de983fdcf5a081978b36
Dvshah13 <dvshah13@gmail.com>
2017-03-23 15:41:55 -0500

    Adding final prediction

--- daily_stock_prediction.py
+++ daily_stock_prediction.py
@@ -171,19 +171,7 @@ def stock_data(symbol):
 
 
     # In[ ]:
-    predicted_rnn = predicted[-1]
-    if predicted_rnn > 0:
-        predicted_rnn = "Positive"
-    else:
-        predicted_rnn = "Negative"
-    accuracy_rnn = accuracy_score(test_labels, predicted)
-    recall_rnn = recall_score(test_labels, predicted)
-    precision_rnn = precision_score(test_labels, predicted)
-    print "Accuracy: ", accuracy_score(test_labels, predicted)
-    print "Recall: ", recall_score(test_labels, predicted)
-    print "Precision: ", precision_score(test_labels, predicted)
-    data_algo_daily = { 'SVM Accuracy': accuracy_svm, 'SVM Precision': precision_svm, 'SVM Recall': recall_svm, 'Predicted SVM': predicted_svm, 'RNN Accuracy': accuracy_rnn, 'RNN Precision': precision_rnn, 'RNN Recall': recall_rnn, 'Predicted RNN': predicted_rnn }
-    return data_algo_daily
+
 
 # In[ ]:
 
@@ -213,38 +201,48 @@ def stock_data(symbol):
 #net.addConnection(FullConnection(net['hidden'], net['output'], name = 'c2'))
 #net.addRecurrentConnection(FullConnection(net['hidden'], net['hidden'], name='c3'))
 #
-# net = buildNetwork(5, 20, 1, hiddenclass = LSTMLayer, outclass = SigmoidLayer, recurrent = True)
-# ds = ClassificationDataSet(5, 1)
-# for i, j in zip(train_features, train_labels):
-#     ds.addSample(i, j)
-#
-#
-# # In[ ]:
-#
-# trainer = BackpropTrainer(net, ds)
-#
-#
-# # In[ ]:
-#
-# epochs = 10
-# for i in range(epochs):
-#     trainer.train()
-#
-#
-# # In[ ]:
-#
-# predicted = list()
-# for i in test_features:
-#     #print net.activate(i)
-#     predicted.append(int(net.activate(i)>0.5))
-# predicted = numpy.array(predicted)
-#
-#
-# # In[ ]:
-#
-# print "Accuracy: ", accuracy_score(test_labels, predicted)
-# print "Recall: ", recall_score(test_labels, predicted)
-# print "Precision: ", precision_score(test_labels, predicted)
+net = buildNetwork(5, 20, 1, hiddenclass = LSTMLayer, outclass = SigmoidLayer, recurrent = True)
+ds = ClassificationDataSet(5, 1)
+for i, j in zip(train_features, train_labels):
+    ds.addSample(i, j)
+
+
+# In[ ]:
+
+trainer = BackpropTrainer(net, ds)
+
+
+# In[ ]:
+
+epochs = 10
+for i in range(epochs):
+    trainer.train()
+
+
+# In[ ]:
+
+predicted = list()
+for i in test_features:
+    #print net.activate(i)
+    predicted.append(int(net.activate(i)>0.5))
+predicted = numpy.array(predicted)
+
+
+# In[ ]:
+
+predicted_rnn = predicted[-1]
+if predicted_rnn > 0:
+    predicted_rnn = "Positive"
+else:
+    predicted_rnn = "Negative"
+accuracy_rnn = accuracy_score(test_labels, predicted)
+recall_rnn = recall_score(test_labels, predicted)
+precision_rnn = precision_score(test_labels, predicted)
+print "Accuracy: ", accuracy_score(test_labels, predicted)
+print "Recall: ", recall_score(test_labels, predicted)
+print "Precision: ", precision_score(test_labels, predicted)
+data_algo_daily = { 'SVM Accuracy': accuracy_svm, 'SVM Precision': precision_svm, 'SVM Recall': recall_svm, 'Predicted SVM': predicted_svm, 'RNN Accuracy': accuracy_rnn, 'RNN Precision': precision_rnn, 'RNN Recall': recall_rnn, 'Predicted RNN': predicted_rnn }
+return data_algo_daily
 #
 #
 # In[ ]:
