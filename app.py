d0fce883a2b163f6bab17e0060a695b87244e509
Dvshah13 <dvshah13@gmail.com>
2017-03-24 10:07:08 -0500

    Adding spy csv

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def figure():
     fig = Figure()
     axis = fig.add_subplot(1, 1, 1)
     # pd.read_csv('/app/daily_historical_prices/spy.csv', nrows=10)
-    df = pd.read_csv('/Users/deepakshah/Documents/Digital Crafts/Machine Learning/Financial Modeling/daily_historical_prices/spy.csv', nrows=20)
+    df = pd.read_csv('/app/daily_historical_prices/spy.csv', nrows=20)
     date_graph = pd.to_datetime(df['Date'])
     adj_close = df['Adj Close']
     xs = date_graph
